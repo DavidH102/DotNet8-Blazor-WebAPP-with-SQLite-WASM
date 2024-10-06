52c1aaff7725bf0d975d474f01d477934c618ce6
DavidH102 <david@britecit.com>
2024-10-06 15:36:12 -0400

    Add loading state and styles to Counter.razor
    - Updated Counter.razor to include a loading state with animation.
    - Added isLoading boolean to manage loading state in Counter.razor.
    - Modified OnInitializedAsync to set isLoading to false after data load.
    - Set minimum logging level to Warning in Program.cs.
    - Included new stylesheet in App.razor.
    - Added Counter.razor.css for styling list of Things and responsive design.

--- src/WebAPP-with-SQLite-WASM/WebAPP-with-SQLite-WASM/WebAPP-with-SQLite-WASM.Client/Program.cs
+++ src/WebAPP-with-SQLite-WASM/WebAPP-with-SQLite-WASM/WebAPP-with-SQLite-WASM.Client/Program.cs
@@ -10,10 +10,11 @@ namespace WebAPP_with_SQLite_WASM.Client
         static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Logging.SetMinimumLevel(LogLevel.Warning);
+
             builder.Services.AddSqliteWasmDbContextFactory<ThingContext>(opts =>
             {
                 opts.UseSqlite("Data Source=things.sqlite3");
-                opts.LogTo(_ => { }, LogLevel.None);  // Disable EF Core logging for performance reasons
             });
             await builder.Build().RunAsync();
         }
